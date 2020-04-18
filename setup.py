c8bfdd41207bbc29832c7faf7c4ba64a93b828f9
Walter Zielenski <walterzielenski@gmail.com>
2020-04-18 11:17:12 -0400

    update name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="alx",
+    name="touchstone",
     version="0.1",
     author="Walter Zielenski",
     author_email="walterzielenski@gmail.com",
